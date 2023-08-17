23ef6743fc098f416faeda9da5e764117f3d68b7
Muhammad Anab Hussain <132558092+MuhammadAnabHussain@users.noreply.github.com>
2023-08-18 00:56:18 +0500

    textutils

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Mybox from "./components/Mybox";
+//import AboutUs from "./components/AboutUs";
+import React, { useState } from "react";
+
 function App() {
+  const [mode, setmode] = useState("light"); //whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#112c54";
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   return (
-   <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">textutils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About us</a>
-        </li>
-        </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    
-   </>
+    <>
+      <Navbar
+        title="textutils"
+        homeText="Home"
+        aboutText="About us"
+        mode={mode} //whether dark mode or light mode
+        toggleMode={toggleMode}
+      />
+      <div className="container my-3">
+        <Mybox heading="Enter the text to Analyze Below " mode={mode} />
+        {/* <AboutUs /> */}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Mybox.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+export default function (props) {
+  const [text, settext] = useState("");
+  const handleUpperCase = () => {
+    // console.log("upper case was clicked");
+    let newText = text.toUpperCase();
+    settext(newText);
+  };
+  const handleLowerCase = () => {
+    // console.log("Lower case was clicked");
+    let newText = text.toLowerCase();
+    settext(newText);
+  };
+  const handlefirstletter = () => {
+    const words = text.split(" ");
+    const capitalizedWords = words.map((word) => {
+      if (word.length > 0) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word; // Handle empty words
+      }
+    });
+    const capitalizedText = capitalizedWords.join(" ");
+    settext(capitalizedText);
+  };
+  const handleClearCase = () => {
+    // console.log("Clear case was clicked");
+    let newText = "";
+    settext(newText);
+  };
+  const handleOnChanged = (event) => {
+    // console.log("on change");
+    settext(event.target.value);
+  };
+
+  return (
+    <>
+      <div
+        className="container "
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChanged}
+            id="myBox"
+            rows="10"
+            style={{
+              color: props.mode === "dark" ? "white" : "dark",
+              backgroundColor: props.mode === "dark" ? "#588cc1" : "white",
+            }}
+          ></textarea>
+        </div>
+
+        <button className="btn btn-primary mx-2" onClick={handleUpperCase}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowerCase}>
+          Convert to lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearCase}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handlefirstletter}>
+          Capitalize
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h2>your summary</h2>
+        <p>
+          <big>{text.split(" ").length} words</big>
+        </p>
+        <p>
+          <big>{text.length} characters</big>
+        </p>
+        <p>
+          <big>{text.split(" ").length * 0.008} minutes read</big>
+        </p>
+        <h2>Preview</h2>
+        <p>{text === "" ? "Enter the text to preview" : text}</p>
+      </div>
+    </>
+  );
+}
